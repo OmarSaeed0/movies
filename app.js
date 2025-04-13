e58adf07f2bf31cd0c540be16d62baff7dc982af
Omar Saeed <OmarSaeed0>
2025-04-13 05:41:35 +0300

    Initial commit

--- app.js
+++ app.js
@@ -2,12 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const tourRoutes = require('./routes/tourRoutes');
-app.use((req, res, next) => {
-res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
-res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-res.header('Access-Control-Allow-Headers', 'Content-Type');
-next();
-});
+
 
 dotenv.config({ path: './.env' });
 
