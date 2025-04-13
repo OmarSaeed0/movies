b533bc23e5554ac3c78c29472b5a242101328ec7
Omar Saeed <OmarSaeed0>
2025-04-13 05:54:52 +0300

    Initial commit

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const app = express();
 app.use(express.json());
 //app.use(express.static(`${__dirname}`));
 app.use((req, res, next) => {
-res.header('Access-Control-Allow-Origin', 'https://movies-g9b2.onrender.com');
-res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-res.header('Access-Control-Allow-Headers', 'Content-Type');
+res.header(Access-Control-Allow-Origin, 'https://movies-g9b2.onrender.com');
+res.header(Access-Control-Allow-Methods, GET, POST, PUT, DELETE);
+res.header(Access-Control-Allow-Headers, Content-Type);
 next();
 });
 
